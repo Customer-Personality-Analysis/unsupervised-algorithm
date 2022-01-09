9100ee6192841c7b08ee031096ad9afc16b83709
bujosa <davidbujosa@gmail.com>
2022-01-09 07:44:45 +0100

    Formatted code

--- src/main.R
+++ src/main.R
@@ -7,16 +7,16 @@
 # Carga de datos y configuración de ambiente
 set.seed(1234)
 dir <- getwd()
-marketingData <- read.delim(file=file.path('data', 'marketing_campaign.csv'), 
-                            stringsAsFactors = FALSE)
+marketing_data <- read.delim(file = file.path("data", "marketing_campaign.csv"),
+ stringsAsFactors = FALSE)
 
-setwd(paste(dir, '/src', sep=''))
-source(file='data.R')
-source(file='unsupervised.R')
+setwd(paste(dir, "/src", sep = ""))
+source(file = "data.R")
+source(file = "unsupervised.R")
 setwd(dir)
 
 # Análisis exploratorio de datos
-data <- Data(data = marketingData)
+data <- Data(data = marketing_data)
 
 data$explore()
 data$clean(removeNA = FALSE)
@@ -24,12 +24,11 @@ data$visualize()
 
 # Ejecución de algoritmos no supervisados
 unsupervised <- Unsupervised(data = data$getRawData())
-algorithms <- c('kmeans')
+algorithms <- c("kmeans")
 
-for (i in 1:length(algorithms)) { 
+for (i in 1:length(algorithms)) {
   algorithm <- unsupervised$getAlgorithm(algorithms[i])
   algorithm$preprocess()
   algorithm$apply()
   algorithm$visualize()
 }
-
