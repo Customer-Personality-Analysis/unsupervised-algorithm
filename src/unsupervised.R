d7c5061050cc0ef7346663e8d55baa90c235ee1b
bujosa <davidbujosa@gmail.com>
2022-01-09 08:40:47 +0100

    Format code in unsupervised class

--- src/unsupervised.R
+++ src/unsupervised.R
@@ -5,26 +5,26 @@
 
 
 library(methods)
-source(file = 'algorithm.R')
-source(file = 'kmeans.R')
-source(file = 'hclust.R')
+source(file = "algorithm.R")
+source(file = "kmeans.R")
+source(file = "hclust.R")
 
-Unsupervised <-
-  setRefClass('unsupervised', fields = list(data = 'data.frame'))
+Unsupervised <- # nolint
+  setRefClass("unsupervised", fields = list(data = "data.frame"))
 
 Unsupervised$methods(
   getAlgorithm = function(name) {
-    'Instanciar la clase para procesar el algoritmo en cuestión'
-    if (name == 'kmeans') {
-      return (Kmeans(data = data))
-    } else if (name == 'hclust') {
-      return (Hclust(data = data))
+    "Instanciar la clase para procesar el algoritmo en cuestión"
+    if (name == "kmeans") {
+      return(Kmeans(data = data))
+    } else if (name == "hclust") {
+      return(Hclust(data = data))
     }
   },
-  
+
   compare = function(results) {
-    'Comparar los clusters en el vector de resultados'
-    
+    "Comparar los clusters en el vector de resultados"
+
     # Ver la similaridad/frequencia entre los clusters
     print(table(map(results, function(algorithm) algorithm$data$cluster)))
   }
